010628777d1ec958468ee4f2c434785fa8183fb3
Mohammed <sajidcurious9@gmail.com>
2023-03-04 03:08:34 +0530

    working with buttons in sideNav component

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copywright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copywright 2022 SM Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack } from "@mui/system";
+import { categories } from "./utils/constants";
+
+const Sidebar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
