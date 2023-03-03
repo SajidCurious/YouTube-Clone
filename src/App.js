d9670303ebb6460e6111e78208c4400b77524a92
Mohammed <sajidcurious9@gmail.com>
2023-03-03 18:05:00 +0530

    building navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return <div className="App">App</div>;
-}
+import React from "react";
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/" element={<VideoDetail />} />
+          <Route path="/" element={<ChannelDetail />} />
+          <Route path="/" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
 export default App;
